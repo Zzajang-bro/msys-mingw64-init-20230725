3cb40127da84cade9539c88b9018e6672f411e72
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-25 19:27:59 +0900

    Update msys-mingw64-init-20230725.sh

--- msys-mingw64-init-20230725.sh
+++ msys-mingw64-init-20230725.sh
@@ -7,3 +7,5 @@ pacman -S mingw-w64-x86_64-gcc
 
 pacman -S mingw-w64-x86_64-toolchain
 ; pacman -S mingw-w64-i686-toolchain      : for x86
+
+pacman -S git mingw-w64-x86_64-toolchain mingw64/mingw-w64-x86_64-SDL2 mingw64/mingw-w64-x86_64-SDL2_mixer mingw64/mingw-w64-x86_64-SDL2_image mingw64/mingw-w64-x86_64-SDL2_ttf mingw64/mingw-w64-x86_64-SDL2_net mingw64/mingw-w64-x86_64-cmake make
