85f559e1c08803611e32e819d191cf4cc20f6737
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-25 19:16:39 +0900

    Update msys-mingw64-init-20230725.sh

--- msys-mingw64-init-20230725.sh
+++ msys-mingw64-init-20230725.sh
@@ -1,5 +1,9 @@
 pacman -Syu
 ; reboot Shell
 pacman -Syu
+
 pacman -S mingw-w64-x86_64-gcc
-; pacman -S mingw-w64-i686-gcc    : for x86
+; pacman -S mingw-w64-i686-gcc            : for x86
+
+pacman -S mingw-w64-x86_64-toolchain
+; pacman -S mingw-w64-i686-toolchain      : for x86
